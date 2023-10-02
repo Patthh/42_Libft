67e0b65efaac96fad783274d0ad6e9c43a9d5995
Patthh <luffinza@gmail.com>
2023-10-02 21:51:13 +0700

    addi. lib

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pracksaw <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 17:44:59 by pracksaw          #+#    #+#             */
-/*   Updated: 2023/10/02 19:48:55 by pracksaw         ###   ########.fr       */
+/*   Updated: 2023/10/02 21:44:27 by pracksaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		len = 0;
+		return (ft_strdup(""));
 	if (s_len - start < len)
 		len = s_len - start;
 	sub = malloc(sizeof(char) * (len + 1));
